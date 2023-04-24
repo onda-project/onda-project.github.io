baf0c6d2916c2206d768d2a79d0074eda6bde6d0
onda-project <>
2023-04-24 03:05:43 +0000

    Update from https://github.com/onda-project/onda-website/commit/81db5f0656b6cc91615c5d78629db4ccbfaeb27c

---
+++ _next/static/IHMp1dYArw9-ekeGKGPaO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fframework.66c3e7013ba1364138b5.js","static\u002Fchunks\u002Fpages\u002Findex-f584d7c52ab61f636395.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-af3fbf48f96d343a18f6.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
